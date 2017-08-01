4e77dd1d4c42b2246dc0136c3d110769b4f7ad72
yenenahmet <yenenahmet@hotmail.com>
2017-08-01 23:32:14 +0300

    Long Distance Path Loss
    Long Distance Path Loss Modeline dayalı Yol Kaybı Simülatörü ## KABLOSUZ AGLAR

---
+++ KablosuzAglar/KablosuzAglar/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace KablosuzAglar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupControl2 = new DevExpress.XtraEditors.GroupControl();
+            this.groupControl3 = new DevExpress.XtraEditors.GroupControl();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
+            this.groupControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl2)).BeginInit();
+            this.groupControl2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl3)).BeginInit();
+            this.groupControl3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Serbest Uzay (n=2)",
+            "Kentsel Alan (n = 2.7-3.5)",
+            "Gölgeli Kentsel Alan (n = 3-5)",
+            "Bina içi,direk görüş var (n = 1.6-1.8)",
+            "Bina içi,direk görüş yok (n = 4-6)",
+            "Fabrika içi,direk görüş yok (n = 3-4)"});
+            this.comboBox1.Location = new System.Drawing.Point(214, 27);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(213, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // groupControl1
+            // 
+            this.groupControl1.Controls.Add(this.textBox2);
+            this.groupControl1.Controls.Add(this.label3);
+            this.groupControl1.Controls.Add(this.simpleButton1);
+            this.groupControl1.Controls.Add(this.textBox1);
+            this.groupControl1.Controls.Add(this.label2);
+            this.groupControl1.Controls.Add(this.label1);
+            this.groupControl1.Controls.Add(this.comboBox1);
+            this.groupControl1.Location = new System.Drawing.Point(12, 12);
+            this.groupControl1.Name = "groupControl1";
+            this.groupControl1.Size = new System.Drawing.Size(442, 167);
+            this.groupControl1.TabIndex = 1;
+            this.groupControl1.Text = "Model";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(214, 114);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 21);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(130, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Patt Loss(dB) :";
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(8, 85);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(419, 23);
+            this.simpleButton1.TabIndex = 4;
+            this.simpleButton1.Text = "Hesapla";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(214, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(213, 21);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(104, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "(Kilometre) Mesafe :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Yayılım Ortamına Bağlı Katsayı Değeri  :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(8, 58);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(401, 308);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // groupControl2
+            // 
+            this.groupControl2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupControl2.Controls.Add(this.label7);
+            this.groupControl2.Controls.Add(this.label4);
+            this.groupControl2.Controls.Add(this.chart1);
+            this.groupControl2.Location = new System.Drawing.Point(460, 12);
+            this.groupControl2.Name = "groupControl2";
+            this.groupControl2.Size = new System.Drawing.Size(448, 398);
+            this.groupControl2.TabIndex = 3;
+            this.groupControl2.Text = "Grafik";
+            // 
+            // groupControl3
+            // 
+            this.groupControl3.Controls.Add(this.textBox6);
+            this.groupControl3.Controls.Add(this.label9);
+            this.groupControl3.Controls.Add(this.textBox5);
+            this.groupControl3.Controls.Add(this.label8);
+            this.groupControl3.Controls.Add(this.textBox3);
+            this.groupControl3.Controls.Add(this.simpleButton2);
+            this.groupControl3.Controls.Add(this.textBox4);
+            this.groupControl3.Controls.Add(this.label5);
+            this.groupControl3.Controls.Add(this.label6);
+            this.groupControl3.Location = new System.Drawing.Point(12, 185);
+            this.groupControl3.Name = "groupControl3";
+            this.groupControl3.Size = new System.Drawing.Size(442, 225);
+            this.groupControl3.TabIndex = 7;
+            this.groupControl3.Text = "Grafik Mesafe Girişi";
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Location = new System.Drawing.Point(8, 85);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(419, 23);
+            this.simpleButton2.TabIndex = 4;
+            this.simpleButton2.Text = "Grafiği Çiz";
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(214, 58);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(121, 21);
+            this.textBox4.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(83, 61);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "(Kilometre) Mesafe Son :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(86, 30);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(122, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "(Kilometre) Mesafe İLK :";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(214, 27);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(121, 21);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoEllipsis = true;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(5, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Patt Loss (dB)  (Y)";
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.AutoEllipsis = true;
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(354, 30);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(80, 16);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Mesafe (X)";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(214, 114);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(121, 21);
+            this.textBox5.TabIndex = 8;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(113, 122);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(95, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "(1) Patt Loss(dB) :";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(214, 141);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(121, 21);
+            this.textBox6.TabIndex = 10;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(113, 144);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(95, 13);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "(2) Patt Loss(dB) :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(954, 476);
+            this.Controls.Add(this.groupControl3);
+            this.Controls.Add(this.groupControl2);
+            this.Controls.Add(this.groupControl1);
+            this.Name = "Form1";
+            this.Text = "Long Distance Path Loss Modeline dayalı Yol Kaybı Simülatörü ";
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
+            this.groupControl1.ResumeLayout(false);
+            this.groupControl1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl2)).EndInit();
+            this.groupControl2.ResumeLayout(false);
+            this.groupControl2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl3)).EndInit();
+            this.groupControl3.ResumeLayout(false);
+            this.groupControl3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private DevExpress.XtraEditors.GroupControl groupControl1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private DevExpress.XtraEditors.GroupControl groupControl2;
+        private DevExpress.XtraEditors.GroupControl groupControl3;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ KablosuzAglar/KablosuzAglar/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KablosuzAglar
+{ // Muhammed Emin KAYABAŞI B130910017 2. öğretim 
+ //    Ahmet YENEN B130910031 1.öğretim
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // n değerlerinin seçilen verilere göre ayarlanması
+                double n = 0;
+                if (comboBox1.Text.Substring(0, 1).Equals("S"))
+                {
+                    n = 2;
+                }
+                else if (comboBox1.Text.Substring(0, 1).Equals("K"))
+                {
+                    n = 3;
+                }
+                else if (comboBox1.Text.Substring(0, 1).Equals("G"))
+                {
+                    n = 4;
+                }
+                else if (comboBox1.Text.Substring(0, 1).Equals("B"))
+                {
+                    n = 1.7;
+                }
+                else if (comboBox1.Text.Substring(0, 1).Equals("b"))
+                {
+                    n = 5;
+                }
+                else if (comboBox1.Text.Substring(0, 1).Equals("F"))
+                {
+                    n = 3.5;
+                }
+                double mesafa = Convert.ToDouble(textBox1.Text);
+                // standart pl = 91.53 ==  20 log10(4pi.d0/lamda)
+                double db = 91.53 + (10 * n) * (System.Math.Log10(mesafa / 1));
+                // Long Distance Path Loss Modeline dayalı Yol Kaybı  Formülü
+                textBox2.Text = Convert.ToDouble(db).ToString();
+                
+            }
+            catch (Exception ex )
+            {
+                MessageBox.Show("Hata oluştu,  N değerini Boş bırakmayınız !!!!");
+                MessageBox.Show(ex.ToString());
+            }
+          
+
+        }
+
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double n = 0;
+                if (comboBox1.Text.Substring(0, 1).Equals("S"))
+                {
+                    n = 2;
+                }
+                else if (comboBox1.Text.Substring(0, 1).Equals("K"))
+                {
+                    n = 3;
+                }
+                else if (comboBox1.Text.Substring(0, 1).Equals("G"))
+                {
+                    n = 4;
+                }
+                else if (comboBox1.Text.Substring(0, 1).Equals("B"))
+                {
+                    n = 1.7;
+                }
+                else if (comboBox1.Text.Substring(0, 1).Equals("b"))
+                {
+                    n = 5;
+                }
+                else if (comboBox1.Text.Substring(0, 1).Equals("F"))
+                {
+                    n = 3.5;
+                }
+                double mesafa_1 = Convert.ToDouble(textBox3.Text);
+                double mesafa_2 = Convert.ToDouble(textBox4.Text);
+
+                double db = 91.53 + (10 * n) * (System.Math.Log10(mesafa_1 / 1));
+                double db2 = 91.53 + (10 * n) * (System.Math.Log10(mesafa_2 / 1));
+                textBox5.Text = Convert.ToDouble(db).ToString();
+                textBox6.Text = Convert.ToDouble(db2).ToString();
+                double dbSonuc;
+                for (double sayac = mesafa_1; sayac <= mesafa_2; sayac = sayac + 1)
+                {
+                    // grafik çizimi
+                    dbSonuc = 91.53 + (10 * n) * (System.Math.Log10(sayac / 1));
+                    chart1.Series[0].Points.AddXY(sayac, dbSonuc);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Hata oluştu,  N değerini Boş bırakmayınız !!!!");
+                MessageBox.Show(ex.ToString());
+            }
+           
+
+
+        }
+    }
+}
